3fd5b72005f4870d04d8e98cd29426ec3abb61f7
Jaay7 <jayakrishna.madem@gmail.com>
2022-04-07 08:12:13 +0530

    direction

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -21,7 +21,7 @@ class _HomeState extends State<Home> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Text('Trackers', style: Theme.of(context).textTheme.headline6),
-            Wrap(
+            Column(
               children: [
                 GestureDetector(
                   onTap: () {
